316e44e080cbcbf431638e1c5aad808dab944367
Merja Neel. <neelmerja@gmail.com>
2025-06-03 18:25:19 +0530

    Delete dotnet_learning-master directory

--- dotnet_learning-master/DTOs/SignInResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BooksApi.DTOs
-{
-    public class SignInResponse
-    {
-        public string AccessToken { get; set; } = string.Empty;
-        public string TokenType { get; set; } = "Bearer";
-    }
-}
